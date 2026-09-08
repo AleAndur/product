a59049f74e90e78ce0429beb0194ad8672870a0f
AleAndur <G1420953@globalbanking.ac.uk>
2026-09-08 19:37:37 +0100

    Cleared weak code.

--- Calculations + activity 4.py
+++ Calculations + activity 4.py
@@ -2,14 +2,14 @@
 x = 33.4
 y = 45.99
 
-sum = x + y
+addition = x + y
 difference = x - y
 product = x * y
 division = x / y
 
-print(sum)
+print(addition)
 print(difference)
 print(product)
 print(division)
-#
+
 
