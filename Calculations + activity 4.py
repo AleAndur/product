10fdc0dae8d99b57cd79307e5662ea11fbef3d83
AleAndur <cosminalexandruionut@gmail.com>
2026-09-08 14:06:19 +0100

    First commit.

---
+++ Calculations + activity 4.py
@@ -0,0 +1,15 @@
+# do activity 4 on this one
+x = 33.4
+y = 45.99
+
+sum = x + y
+difference = x - y
+product = x * y
+division = x / y
+
+print(sum)
+print(difference)
+print(product)
+print(division)
+
+
